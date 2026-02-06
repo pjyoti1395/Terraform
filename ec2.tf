8dd98e82d3fa26f356555a145c41e7455e6863d6
JyotiPanigrahi <panigrahij95@gmail.com>
2026-02-06 15:06:26 +0530

    removed the data block from ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,20 +1,4 @@
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["amazon"]
-}
-
 resource "aws_instance" "my_ec2" {
   ami                     = data.aws_ami.amazon_linux.id
   instance_type           = var.instance_type
