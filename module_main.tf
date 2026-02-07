56586acfefb7c33a8da5fe1fed1247454f5b90ef
JyotiPanigrahi <panigrahij95@gmail.com>
2026-02-07 20:43:09 +0530

    Added values to key_name and private_path

--- module_main.tf
+++ module_main.tf
@@ -6,7 +6,7 @@ module "ec2_dev" {
   security_group_id = aws_security_group.my_sg.id
   instance_name     = "dev-ec2"
   key_name          = "key123"
-  private_key_path  = "private_keys"
+  private_key_path  = "/home/ec2-user/key123.pem"
 }
 
 module "ec2_test" {
@@ -17,7 +17,7 @@ module "ec2_test" {
   security_group_id = aws_security_group.my_sg.id
   instance_name     = "test-ec2"
   key_name          = "key123"
-  private_key_path  = "private_keys"
+  private_key_path  = "/home/ec2-user/key123.pem"
 
 }
 
--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "Terraform_Module_EC2" {
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
+  key_name               = var.key_name
 
   tags = {
     Name = var.instance_name
