5ef4b631a6500e2a3dab631ec9dacd3db59d7952
JyotiPanigrahi <panigrahij95@gmail.com>
2026-02-08 00:03:56 +0530

    up

--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -2,9 +2,6 @@ output "instance_id" {
   value = aws_instance.Terraform_Module_EC2.id
 }
 
-output "public_ip" {
-  value = aws_instance.Terraform_Module_EC2.public_ip
-}
 
 output "ami-image" {
   value = data.aws_ami.amazon_linux.id
