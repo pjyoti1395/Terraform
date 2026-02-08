eca010c09e6f2ab9e8efc947ec4b1c83a1898a75
JyotiPanigrahi <panigrahij95@gmail.com>
2026-02-08 15:35:55 +0530

    up

---
+++ modules/depends_on/main.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "example" {
+  ami           = "ami-02781fbdc79017564"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-123456"
+
+  # Force dependency even if not referenced
+  depends_on = [
+    aws_security_group.my_sg
+  ]
+
+  tags = {
+    Name = "demo-instance"
+  }
+}
