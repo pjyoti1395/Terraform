852ff94c05e0d24302a9df34a383a617deadfbb6
JyotiPanigrahi <panigrahij95@gmail.com>
2026-02-08 21:32:05 +0530

    up

---
+++ modules/lifecycle/ignore_changes/ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "demo_ignore" {
+  ami           = "ami-02781fbdc79017564"
+  instance_type = "t3.small"
+  subnet_id     = aws_subnet.my_subnet.id
+
+  tags = {
+    Name = "ignore-demo"
+    Env  = "dev"
+  }
+}
