369e4756cad84ee8b9e494c1f996dc4bdb3af835
JyotiPanigrahi <panigrahij95@gmail.com>
2026-02-08 16:30:14 +0530

    up

--- modules/depends_on/main.tf
+++ modules/depends_on/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "depends_on_demo" {
   ami           = "ami-02781fbdc79017564"
   instance_type = "t3.small"
-  subnet_id     = "subnet-123456"
+  subnet_id = aws_subnet.my_subnet.id 
 
   # Force dependency even if not referenced
   depends_on = [
@@ -10,5 +10,20 @@ resource "aws_instance" "depends_on_demo" {
 
   tags = {
     Name = "demo-instance"
+  }
+}
+
+
+resource "aws_instance" "depends_on_demo_2" {
+  ami           = "ami-02781fbdc79017564"
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.my_subnet.id
+
+  depends_on = [
+    aws_security_group.my_sg
+  ]
+
+  tags = {
+    Name = "demo-instance-2"
   }
 }
